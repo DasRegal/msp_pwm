b7803f92762d5bfceaf4bdcb7856801feab261a7
DasRegal <plexus.9310@gmail.com>
2013-08-05 13:04:37 +0700

    [*] msp_pwm: new board v3

--- main.c
+++ main.c
@@ -26,20 +26,33 @@
   #define   T_CHANNEL     (20000 / N_CHANNELS)
   #define   LOCK_CHAN     (isLock = 1)
   #define   UNLOCK_CHAN   (isLock = 0)
+  #define   INT_PORT_ON   (P2IE |= CS)
+  #define   INT_PORT_OFF  (P2IE &= ~CS)
+  #define   INT_USI_ON    (USICTL1 |=  USIIE)
+  #define   INT_USI_OFF   (USICTL1 &= ~USIIE)
+
+#define   CS      BIT7
+#define   LED     BIT6
+#define   MISO    BIT6
+#define   MOSI    BIT7
+#define   CLK     BIT5
 
 struct SChan
 {
   int isPulse;
   int data;
   int dx;
+  int isHiByte;
 };
 
-struct SChan chan[N_CHANNELS];
+volatile struct SChan chan[N_CHANNELS];
 
-int indexChan;
-int indexByte;
-int isLock;
-int dx_temp;
+volatile int indexChan;
+volatile int indexByte;
+volatile int isLock;
+volatile int dx_temp;
+volatile int chanel;
+volatile int isChan;
 
 void InitPWM(int n)
 {
@@ -49,12 +62,14 @@ void InitPWM(int n)
   {
     chan[i].isPulse = 1;
     chan[i].dx = 1000;
+    chan[i].isHiByte = 1;
     P1DIR |= 1 << i;
     P1OUT &= ~(1 << i);
   }
   indexChan = 0;
   indexByte = 0;
   isLock = 0;
+  isChan = 1;
 }
 
 void main(void)
@@ -70,7 +85,8 @@ void main(void)
   DCOCTL = CALDCO_1MHZ;
 
   USICTL0 |= USIPE7 + USIPE5 + USIOE; // Port, SPI slave
-  USICTL1 |= USIIE;                     // Counter interrupt, flag remains set
+  // USICTL1 |= USIIE;                     // Counter interrupt, flag remains set
+  INT_USI_OFF;
   USICTL0 &= ~USISWRST;                 // USI released for operation
   USISRL = 0xFE;
   USICNT = 8;
@@ -85,10 +101,31 @@ void main(void)
                    // TACLR - начальное обнуление таймера 
 
   InitPWM(N_CHANNELS);
-  P1DIR |= BIT6;
-  P1OUT &= ~BIT6;
+  // P1DIR |= BIT6;
+  // P1OUT &= ~BIT6;
 
-  _BIS_SR(LPM0_bits + GIE);             // Enter LPM0 w/ interrupt
+  P2DIR &= ~CS;
+  P2SEL &= ~CS;
+  P2OUT |= CS;
+  P2REN |= CS;
+  P2IES &= ~CS;
+  P2IFG &= ~CS;
+
+  P2SEL &= ~LED;
+  P2DIR |= LED;
+  P2OUT |= LED;
+  P2OUT ^= LED;
+
+  P1DIR  &= ~(MOSI + CLK);
+  P1REN |= MOSI + CLK;
+
+
+  P2OUT |= LED;
+  INT_PORT_ON;
+
+  P1DIR &= ~MISO;
+
+  _BIS_SR(GIE);             // Enter LPM0 w/ interrupt
   while(1);
 }
 
@@ -97,33 +134,29 @@ __interrupt void universal_serial_interface(void)
 {
   int data;
   data = USISRL & 0xFF;
-
-  if (indexByte <= 1 && data == 0xFF) 
+  if (isChan)
   {
-    indexByte++;
-    if (indexByte == 1 && data != 0xFF) indexByte = 1;
+    chanel = data;
+    isChan = 0;
   }
   else
-  if ((indexByte < (N_CHANNELS * 2 + 2)) && indexByte > 1)
   {
-    if (!(indexByte % 2))
+    if (chan[chanel].isHiByte)
     {
+      chan[chanel].isHiByte = 0;
       dx_temp = data << 8;
     }
     else
     {
-      LOCK_CHAN;
-      chan[(indexByte - 3) / 2].dx = dx_temp + data;
-      UNLOCK_CHAN;
+      chan[chanel].dx = dx_temp + data;
+      chan[chanel].isHiByte = 1;
+      isChan = 1;   
     }
-
-    if (indexByte == (N_CHANNELS * 2 + 1)) indexByte = 0;
-    else indexByte++;
   }
-  else indexByte = 0;
-
-  USISRL = 0xFE;
+ 
+  USISRL = 0x00;
   USICNT = 8;
+  P2OUT ^= LED;
 }
 
 // 
@@ -152,3 +185,31 @@ __interrupt void CCR0_ISR(void)
     WDTCTL = WDTPW + WDTCNTCL;
   }  
 } // CCR0_ISR
+
+#pragma vector=PORT2_VECTOR
+__interrupt void Port_1(void)
+{
+  INT_PORT_OFF;
+
+  // Переход CS 1 -> 0
+  if (P2IES & CS)
+  {
+    // линию DO - выход
+    // P1DIR |= MISO;
+    INT_USI_ON;
+  }
+  else
+  { 
+    // линию DO - вход
+    // дабы исключить КЗ
+    // P1DIR &= ~MISO;
+    P2OUT |= LED;
+    USICTL1 &= ~USIIFG;
+    isChan = 1;
+    INT_USI_OFF;
+  }
+
+  P2IFG &= ~CS;
+  P2IES ^= CS;
+  INT_PORT_ON;
+}
